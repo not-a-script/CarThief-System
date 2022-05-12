e398ba15852aa0318fe11baabe5069547a966e5a
Space V <40030799+TheSpaceGamerV2@users.noreply.github.com>
2022-05-12 11:19:09 +0200

    tweak(fxmanifest): reformat

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 game { 'gta5' }
 
 shared_scripts {
-	'@es_extended/imports.lua',
+    '@es_extended/imports.lua',
     '@ox_lib/init.lua',
     'shared/config.lua',
 }
@@ -17,18 +17,18 @@ server_scripts {
 
 client_scripts {
     "@RageUI/RageUI.lua",
-	"@RageUI/Menu.lua",
-	"@RageUI/MenuController.lua",
-	"@RageUI/components/Audio.lua",
-	"@RageUI/components/Graphics.lua",
-	"@RageUI/components/Keys.lua",
-	"@RageUI/components/Util.lua",
-	"@RageUI/components/Visual.lua",
-	"@RageUI/elements/ItemsBadge.lua",
-	"@RageUI/elements/ItemsColour.lua",
-	"@RageUI/elements/PanelColour.lua",
-	"@RageUI/items/Items.lua",
-	"@RageUI/items/Panels.lua",
+    "@RageUI/Menu.lua",
+    "@RageUI/MenuController.lua",
+    "@RageUI/components/Audio.lua",
+    "@RageUI/components/Graphics.lua",
+    "@RageUI/components/Keys.lua",
+    "@RageUI/components/Util.lua",
+    "@RageUI/components/Visual.lua",
+    "@RageUI/elements/ItemsBadge.lua",
+    "@RageUI/elements/ItemsColour.lua",
+    "@RageUI/elements/PanelColour.lua",
+    "@RageUI/items/Items.lua",
+    "@RageUI/items/Panels.lua",
 
 
     'client/stealveh_cl.lua',
@@ -38,5 +38,5 @@ client_scripts {
 dependencies {
     'es_extended',
     'ox_lib',
-	'RageUI',
+    'RageUI',
 }
